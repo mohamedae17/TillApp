7cac3280840fa6e501b82f317f47d2fa5c810993
Mohamed Ahmed Mohamed Ibrahim Afifi <mamibrahim@ejada.com>
2025-01-29 03:32:05 +0200

    add Controlers and DI Logging

--- TillApp.Server/Program.cs
+++ TillApp.Server/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddLogging(logging =>
+{
+    logging.AddConsole();
+    logging.AddDebug();
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
