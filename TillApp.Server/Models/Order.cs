d728240d83bd0fb60914d7936b8dfe0bb5cd80a2
Mohamed Ahmed Mohamed Ibrahim Afifi <mamibrahim@ejada.com>
2025-01-29 02:21:09 +0200

    Add AppDbContext and Models

---
+++ TillApp.Server/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace TillApp.Server.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string OrderName { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsPaid { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ TillApp.Server/Models/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TillApp.Server.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemID { get; set; }
+        public int OrderID { get; set; }
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+        public Order Order { get; set; }
+    }
+}
