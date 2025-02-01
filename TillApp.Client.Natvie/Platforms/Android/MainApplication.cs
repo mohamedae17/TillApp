2128918f3a43ac1daf961b0d0d745bfda22d95a4
Mohamed Ahmed Mohamed Ibrahim Afifi <mamibrahim@ejada.com>
2025-02-01 18:29:25 +0200

    Add network_secrity for enable localhost API

--- TillApp.Client.Natvie/Platforms/Android/MainApplication.cs
+++ TillApp.Client.Natvie/Platforms/Android/MainApplication.cs
@@ -3,7 +3,7 @@ using Android.Runtime;
 
 namespace TillApp.Client.Natvie
 {
-    [Application]
+    [Application(UsesCleartextTraffic = true)]
     public class MainApplication : MauiApplication
     {
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
